fd2d23fa93c2a9c57b3a851014e7f2ab0d48d3cc
bluwy <bjornlu.dev@gmail.com>
2022-05-17 20:38:01 +0800

    ci: 2 threads

--- vitest.config.e2e.ts
+++ vitest.config.e2e.ts
@@ -2,7 +2,6 @@ import { resolve } from 'path'
 import { defineConfig } from 'vitest/config'
 
 const timeout = process.env.CI ? 50000 : 30000
-const ciMaxThreads = process.platform === 'linux' ? 2 : 1
 
 export default defineConfig({
   resolve: {
@@ -22,7 +21,7 @@ export default defineConfig({
       if (log.match(/experimental|jit engine|emitted file|tailwind/i))
         return false
     },
-    maxThreads: process.env.CI ? ciMaxThreads : undefined,
+    maxThreads: process.env.CI ? 2 : undefined,
     minThreads: process.env.CI ? 1 : undefined
   },
   esbuild: {
